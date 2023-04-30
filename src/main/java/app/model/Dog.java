2db04841664cc35bfac3066a1eb71e04f4e79112
unknown <belousnata00@gmail.com>
2023-04-30 11:31:34 +0300

    FIRST

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,22 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+//    private String dog;
+//
+//    public String getDog() {
+//        return dog;
+//    }
+//
+//    public void setDog(String dog) {
+//        this.dog = dog;
+//    }
+
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
+
